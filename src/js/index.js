45dad143a3360449e7320adfcfd6fb4576685f1c
Henry Desroches <yo@henry.codes>
2022-11-04 15:52:37 -0400

    🗑 - Temporarily remove transitions JS

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,13 @@
-import * as Taxi from '@unseenco/taxi';
+// import * as Taxi from '@unseenco/taxi';
 
-import DefaultTransition from './transitions/default';
-import BookTransition from './transitions/book.transition';
+// import DefaultTransition from './transitions/default';
+// import BookTransition from './transitions/book.transition';
 
-const taxi = new Taxi.Core({
-  transitions: {
-    default: DefaultTransition,
-    toBook: BookTransition,
-  },
-});
+// const taxi = new Taxi.Core({
+//   transitions: {
+//     default: DefaultTransition,
+//     toBook: BookTransition,
+//   },
+// });
 
-taxi.addRoute('/', '.*', 'toBook');
+// taxi.addRoute('/', '.*', 'toBook');
