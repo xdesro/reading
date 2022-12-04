7e0882215fbc9ee51969fb481821c69721a2de0a
Henry Desroches <yo@henry.codes>
2022-12-04 13:20:59 -0500

    🩹 - Temporarily fix missing homepage images (worse perf, big img)

--- src/_includes/NowReading.vue
+++ src/_includes/NowReading.vue
@@ -5,7 +5,7 @@
     <h3 class="now__book" v-else>N/A</h3>
     <ul class="now__stack" v-if="list.length">
       <li v-for="book in list" :key="book.isbn">
-        <img class="now__cover" :src="`/img/${book.isbn}.jpg`" alt="" />
+        <img class="now__cover" :src="`/covers/${book.isbn}.jpg`" alt="" />
       </li>
     </ul>
   </div>
